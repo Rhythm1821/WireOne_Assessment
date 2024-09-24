20c0e2054e5005e8b49df47ec104cebda95882da
Rhythm1821 <alamrawat1821@gmail.com>
2024-09-24 12:00:06 +0530

    registering pricing and log configs to the admin

--- pricing/admin.py
+++ pricing/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import PricingConfig, PricingConfigLog
 
 # Register your models here.
+@admin.register(PricingConfig)
+class PricingConfigAdmin(admin.ModelAdmin):
+    list_display = ('day_of_week', 'base_distance_price', 'additional_price_per_km','time_multiplier_factor', 'enabled')
+    list_filter = ('day_of_week', 'enabled')
+    search_fields = ('day_of_week', )
+
+    def save_model(self,request,obj,form,change):
+        super().save_model(request,obj,form,change)
+
+        PricingConfigLog.objects.create(
+            config=obj,
+            modified_by=request.user.username,
+            action='Updated' if change else 'Created'
+        )
+    
+    def delete_model(self,request,obj):
+        PricingConfigLog.objects.create(
+            config=obj,
+            modified_by=request.user.username,
+            action='Deleted'
+        )
+
+        super().delete_model(request,obj)
+
+
+@admin.register(PricingConfigLog)
+class PricingConfigLogAdmin(admin.ModelAdmin):
+    list_display = ('config', 'timestamp', 'action', 'modified_by')
+    readonly_fields = ('timestamp', )
+    search_fields = ('modified_by', )
